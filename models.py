8ce57dc325bbba5d08d76cb8274a7b50ae18568b
saran53rithu <saran53rithu@gmail.com>
2024-09-15 08:50:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,23 +3,21 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-#database_url = "postgresql://postgres:password@localhost:5432/postgres"
-
 database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+# referenced the implementation from fyyur project https://github.com/udacity/cd0046-SQL-and-Data-Modeling-for-the-Web/blob/master/app.py and also my local fyyur project
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
+# referenced from https://learn.udacity.com/nanodegrees/nd0044/parts/cd0046/lessons/27a1ef74-4a77-4651-aca2-d701a408c3fd/concepts/cf6671dd-5249-4215-8207-dd9c677dc787?lesson_tab=lesson
 
 
 class Restaurant(db.Model):
@@ -30,8 +28,6 @@ class Restaurant(db.Model):
     address = db.Column(db.String(255), nullable=False)
     phone_number = db.Column(db.String(15), nullable=True)
     email = db.Column(db.String(255), nullable=True)
-    
-    # Relationship with Menu table; no backref to avoid conflicts
     menus = db.relationship('Menu', back_populates='restaurant', lazy=True, cascade="all, delete-orphan")
 
     def __repr__(self):
@@ -46,8 +42,6 @@ class Menu(db.Model):
     description = db.Column(db.Text, nullable=True)
     available = db.Column(db.Boolean, default=True, nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
-    
-    # Relationship with Restaurant table; no backref to avoid conflicts
     restaurant = db.relationship('Restaurant', back_populates='menus')
 
     def __repr__(self):
