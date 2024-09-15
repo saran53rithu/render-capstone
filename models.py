fc719f60fb8c96c6fb46adfbfa82851f876a5b4b
saran53rithu <saran53rithu@gmail.com>
2024-09-15 11:58:30 +0530

    Update models.py

--- models.py
+++ models.py
@@ -33,6 +33,37 @@ class Restaurant(db.Model):
     def __repr__(self):
         return f'<Restaurant(name={self.name}, address={self.address}, phone_number={self.phone_number}, email={self.email})>'
 
+    @classmethod
+    def insert(cls, name, address, phone_number, email):
+        restaurant = cls(name=name, address=address, phone_number=phone_number, email=email)
+        db.session.add(restaurant)
+        db.session.commit()
+        return restaurant
+
+    @classmethod
+    def update(cls, restaurant_id, name=None, address=None, phone_number=None, email=None):
+        restaurant = cls.query.get(restaurant_id)
+        if not restaurant:
+            return None
+        if name is not None:
+            restaurant.name = name
+        if address is not None:
+            restaurant.address = address
+        if phone_number is not None:
+            restaurant.phone_number = phone_number
+        if email is not None:
+            restaurant.email = email
+        db.session.commit()
+        return restaurant
+
+    @classmethod
+    def delete(cls, restaurant_id):
+        restaurant = cls.query.get(restaurant_id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+        return restaurant
+
 class Menu(db.Model):
     __tablename__ = 'menu'
     
@@ -46,3 +77,11 @@ class Menu(db.Model):
 
     def __repr__(self):
         return f'<Menu(name={self.name}, price={self.price}, description={self.description}, available={self.available})>'
+
+    @classmethod
+    def insert(cls, name, price, description, available, restaurant_id):
+        menu = cls(name=name, price=price, description=description, available=available, restaurant_id=restaurant_id)
+        db.session.add(menu)
+        db.session.commit()
+        return menu
+
