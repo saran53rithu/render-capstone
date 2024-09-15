ffe00ef72ce3d67cc541e055a1af5429f52f8e93
saran53rithu <saran53rithu@gmail.com>
2024-09-15 07:07:44 +0530

    Update models.py

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-def setup_db(app, database_url=database_url):
+def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
